67d51755fd664cd1669d5128e8842fe80b772760
Viktor Bergmark <viktor.bergmark@outlook.com>
2026-01-19 17:29:58 +0100

    add markdown to textblocks conversion

--- src/markdown_extract.py
+++ src/markdown_extract.py
@@ -11,3 +11,11 @@ def extract_markdown_links(text):
     return  re.findall(link_pattern, text)
 
 
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    final_blocks = []
+    for block in blocks:
+        block = block.strip()
+        if block != "":
+            final_blocks.append(block)
+    return final_blocks
