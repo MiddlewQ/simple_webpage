cd60d159eb05cc2f48e5bb3fa44a7d0544b6198f
Viktor Bergmark <viktor.bergmark@outlook.com>
2026-01-20 21:53:58 +0100

    fix: inline code is one backtick

--- src/text_to_nodes.py
+++ src/text_to_nodes.py
@@ -79,7 +79,7 @@ def text_to_textnodes(text):
     node = TextNode(text=text, text_type=TextType.TEXT)
     nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
     nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
-    nodes = split_nodes_delimiter(nodes, "```", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
