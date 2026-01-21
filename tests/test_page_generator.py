98fb43b5682e48092b1427f6d82ae9c9fca54258
Viktor Bergmark <viktor.bergmark@outlook.com>
2026-01-21 15:29:30 +0100

    add: extract title function

--- tests/test_page_generator.py
+++ tests/test_page_generator.py
@@ -13,9 +13,17 @@ class TestPageGeneration(unittest.TestCase):
     def test_invalid_heading(self):
         md = "## Wrong heading"
         with self.assertRaises(ValueError):
+<<<<<<< HEAD
             title = extract_title(md)
+=======
+            _ = extract_title(md)
+>>>>>>> 3c69d67 (add: extract title function)
 
     def test_missing_heading(self):
         md = "This text is missing its heading"
         with self.assertRaises(ValueError):
+<<<<<<< HEAD
             title = extract_title(md)
+=======
+            _ = extract_title(md)
+>>>>>>> 3c69d67 (add: extract title function)
