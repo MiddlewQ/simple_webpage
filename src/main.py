572a5ffb819667d856c4deb389044cb5f4a63820
Viktor Bergmark <viktor.bergmark@outlook.com>
2026-01-21 15:01:14 +0100

    add: main copies static into public

--- src/main.py
+++ src/main.py
@@ -1,17 +1,43 @@
-from src.markdown_to_html_node import block_to_html_quote, markdown_to_blocks
-from .textnode import TextNode
+import os, shutil, sys
+
+from src.markdown_to_html_node import markdown_to_html_node
+
+def clean_directory(directory):
+    if not os.path.isdir(directory):
+        print(f"Directory {directory} not found", file=sys.stderr)
+        sys.exit(1)
+    try:
+        shutil.rmtree(directory)
+    except OSError as e:
+        print(f"Failed to delete {directory}: {e}", file=sys.stderr)
+        sys.exit(1)
+
+def copy_directory(source_directory, destination_directory):
+    if not os.path.isdir(source_directory):
+        print(f"Source directory {source_directory} not found")
+        sys.exit(1)
+    if not os.path.isdir(destination_directory):
+        print(f"Destination directory {destination_directory} not found")
+        sys.exit(1)
+    
+    for entity in os.listdir(source_directory):
+        source_filepath = os.path.join(source_directory, entity)
+        destination_filepath = os.path.join(destination_directory, entity)
+        if os.path.isfile(source_filepath):
+            shutil.copy(source_filepath, destination_filepath)
+        elif os.path.isdir(source_filepath):
+            os.mkdir(destination_filepath)
+            copy_directory(source_filepath, destination_filepath)
+
 
 def main():
-        md = """
-> a line
-> b line
-> c line
-"""
-        block = markdown_to_blocks(md)[0]
-        node = block_to_html_quote(block)
-        html = node.to_html()
-        print(node)
-        # self.assertEqual("<blockquote>a line\nb line\nc line</blockquote>", html)
+    source = "static"
+    destination = "public"
+    clean_directory(destination)
+    os.mkdir(destination)
+    copy_directory(source, destination)
+   
+    pass
 
 
 if __name__ == "__main__":
